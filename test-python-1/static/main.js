ebd9c878ad33f5beec32e9ead13ef34fc0c8cfe1
SkohTV <skookie.omg.lol@gmail.com>
2024-03-04 05:27:20 +0100

    100 lighthouse !

--- test-python-1/static/main.js
+++ test-python-1/static/main.js
@@ -1,3 +1,3 @@
-!function(){
-  document.getElementById('js-test').textContent = 'JS is working !';
-}
+window.onload = () => {                                                                                                                                                                                           │
+  document.getElementById('js-test').textContent = 'JS is working !';                                                                                                                                             │
+}                                                                                                                                                                                                                 │
