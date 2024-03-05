2f4948562fd03e32850c75c6ab3d6a74a6b1573d
Skoh <skookie.omg.lol@gmail.com>
2024-03-05 04:49:31 +0100

    basic page

--- test-python-1/static/ae/script.js
+++ test-python-1/static/ae/script.js
@@ -1,3 +1,10 @@
 window.onload = () => {
   document.getElementById('js-test').textContent = 'JS is working !';
+
+  // const arr = document.getElementsByClassName('img-background')
+  // for (let x of arr) {
+  //   let rnd = 1 + Math.floor(Math.random() * 77);
+  //   x.style.backgroundImage = `url(./imgs/crab_pic/${rnd}.jpg)`;
+  // }
+
 }
