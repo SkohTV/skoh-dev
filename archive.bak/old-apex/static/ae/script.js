d50b04263b424df9fba99cd336de225bd9e5326a
Skoh <qlpth+github@proton.me>
2024-05-07 12:14:39 +0200

    moved old web servers into archive.bak

---
+++ archive.bak/old-apex/static/ae/script.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+
+  const arr = document.getElementsByClassName('img-background')
+  for (let x of arr) {
+    let rnd = 1 + Math.floor(Math.random() * 77);
+    x.style.backgroundImage = `url(/ae/i/crab/${rnd})`;
+  }
+
+}
+
+function playsound(number){
+  let item = document.getElementById(`music${number}`);
+  item.paused ? item.play() : item.pause();
+}
---
+++ archive.bak/old-apex/static/main.js
@@ -0,0 +1,3 @@
+window.onload = () => {
+  document.getElementById('js-test').textContent = 'JS is working !';
+}
