b9fe0af37fbb1cbe0bd4dee6ae793a110eda1f6b
Skoh <qlpth+github@proton.me>
2024-07-26 09:55:50 +0200

    small optimization of cron.sh

--- cron.sh
+++ cron.sh
@@ -4,13 +4,15 @@ cd /home/debian/skoh-dev
 git fetch
 
 
+# If branch out of date
 if git status | grep -q 'Your branch is behind'; then
   echo "Updating repo skoh-dev"
 
+  # Remove all non-commited modifs
   git reset --hard
   git pull
 
-  docker compose down
-  docker compose build
-  docker compose up -d
+  docker compose build # rebuild
+  docker compose down # down
+  docker compose up -d # up
 fi
