5a6f57c2cae814bc476a01d28179b11153b211b0
SkohTV <qlpth+github@proton.me>
2025-01-31 19:13:28 +0100

    added blog button

--- apex/app/_header.js
+++ apex/app/_header.js
@@ -10,8 +10,8 @@ export default Header;
 
 function Header() {
   return (
-    <header className="font-bold flex justify-center items-center border-b border-zinc-700/75 border-solid">
-      <div className="sm:absolute sm:left-0 sm:top-0 m-2 h-fit w-fit rounded-full border-2 border-purple-700 bg-zinc-900">
+    <header className="font-bold flex justify-between items-center border-b border-zinc-700/75 border-solid">
+      <div className="m-2 h-fit w-fit rounded-full border-2 border-purple-700 bg-zinc-900">
         <Link href="/">
           <Image src={logo} width={44} height={44} alt="Skoh's logo" />
         </Link>
@@ -21,6 +21,9 @@ function Header() {
         <HeaderLink name="Skills" url="/skills" />
         <HeaderLink name="Toolbox" url="/toolbox" />
         <HeaderLink name="Projects" url="/projects" />
+      </div>
+      <div className="flex space-x-4 p-5">
+        <BlogLink name="Blog" url="https://blog.skoh.dev" />
       </div>
     </header>
   );
@@ -32,6 +35,18 @@ function HeaderLink({ name, url }) {
     <Link
       href={url}
       className="hover:bg-violet-600 duration-100 px-1"
+    >
+      {name}
+    </Link>
+  );
+}
+
+
+function BlogLink({ name, url }) {
+  return (
+    <Link
+      href={url}
+      className="hover:bg-red-600 duration-100 px-1"
     >
       {name}
     </Link>
