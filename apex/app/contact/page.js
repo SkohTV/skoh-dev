9284cc2c562dc5221a1a3ccbd699962b52486bc1
SkohTV <qlpth+github@proton.me>
2025-08-07 17:34:46 +0200

    apex: added contact info (wip)

--- apex/app/contact/page.js
+++ apex/app/contact/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default Contact;
 
 export const metadata = {
@@ -10,7 +12,22 @@ function Contact() {
   return (
     <div>
       <h1>Contact</h1>
-      <p>Blabla</p>
+      <p>Discord, at <PLink url="https://discord.com/users/373055398464323584">@skoh</PLink></p>
+      <p>Mail, at <PLink url="mailto:contact@skoh.dev">contact@skoh.dev</PLink></p>
+      <p>Phone, at <PLink url="tel:+330601020304">+33 6 01 02 03 04</PLink> (fake number, wip)</p>
     </div>
+  );
+}
+
+
+
+function PLink({ children, url }) {
+  return (
+    <Link
+      href={url}
+      className="font-bold"
+    >
+      {children}
+    </Link>
   );
 }
