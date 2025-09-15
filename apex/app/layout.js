40424199b5eaa74ae59fa7c59a6d25993481ffc6
SkohTV <contact@skoh.dev>
2025-09-15 11:48:08 -0400

    apex: reduce default text size

--- apex/app/layout.js
+++ apex/app/layout.js
@@ -6,7 +6,7 @@ import "@/styles/globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-zinc-900 text-white flex flex-col h-screen">
+      <body className="bg-zinc-900 text-white flex flex-col h-screen text-sm">
         <Header />
         <div className="p-5 flex-grow">{children}</div>
         <Footer />
