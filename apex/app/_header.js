58b7fd19a02198104961175c3764f767e16ba79e
SkohTV <contact@skoh.dev>
2025-09-17 19:12:44 -0400

    apex: better centering of header

--- apex/app/_header.js
+++ apex/app/_header.js
@@ -5,17 +5,18 @@ import { AboutSVG, ToolboxSVG, ProjectsSVG, ContactSVG } from "./_svg";
 
 export default function Header() {
   return (
-    <header className="font-bold flex justify-between items-center border-b border-zinc-700/75 border-solid">
+    <header className="font-bold flex justify-start items-center border-b border-zinc-700/75 border-solid">
       <div className="flex space-x-4 p-5">
         <Link href="/" className="hover:bg-violet-600 duration-100 px-1 stroke-white text-xl">Skøh</Link>
       </div>
-      <div className="flex space-x-4 p-5">
-        <HeaderLink name="About" icon={AboutSVG({ size: 24 })} url="/about" />
-        <HeaderLink name="Toolbox" icon={ToolboxSVG({ size: 24 })} url="/toolbox" />
-        <HeaderLink name="Projects" icon={ProjectsSVG({ size: 24 })} url="/projects" />
-        <HeaderLink name="Contact" icon={ContactSVG({ size: 24 })} url="/contact" />
+      <div className="absolute left-1/2 transform-[translateX(-50%)]">
+        <div className="flex space-x-4 p-5">
+          <HeaderLink name="About" icon={AboutSVG({ size: 24 })} url="/about" />
+          <HeaderLink name="Toolbox" icon={ToolboxSVG({ size: 24 })} url="/toolbox" />
+          <HeaderLink name="Projects" icon={ProjectsSVG({ size: 24 })} url="/projects" />
+          <HeaderLink name="Contact" icon={ContactSVG({ size: 24 })} url="/contact" />
+        </div>
       </div>
-      <div className="hidden sm:flex space-x-4 p-5"></div>
     </header>
   );
 }
