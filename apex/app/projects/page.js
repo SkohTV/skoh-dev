6bba80bbe083d9f8988b0823c0130ec5adc78375
SkohTV <contact@skoh.dev>
2025-09-28 12:12:28 -0400

    apex: change projects

--- apex/app/projects/page.js
+++ apex/app/projects/page.js
@@ -9,11 +9,22 @@ function Projects() {
   return (
     <div>
       <h1>Projects</h1>
-      <p>* Quantum (wip)</p>
-      <p>* Remote Desktop (wip)</p>
-      <p>* ocl-rs</p>
-      <p>* r-pi onlyoffice (todo)</p>
-      <p>* Open Source (wip)</p>
+
+      <div>
+        <h2>Featured</h2>
+        <p>* Quantum (wip)</p>
+        <p>* ocl-rs</p>
+        <p>* Open Source (wip)</p>
+      </div>
+
+      <div>
+        <h2>Small</h2>
+        <p>* Dotfiles</p>
+        <p>* Remote Desktop (wip)</p>
+        <p>* skoh-dev</p>
+        <p>* skoh-tv</p>
+      </div>
+
     </div>
   );
 }
