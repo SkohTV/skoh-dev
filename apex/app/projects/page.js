4370fd38536a9c2d5957a02fa2c64a0973a87d35
SkohTV <contact@skoh.dev>
2025-11-02 13:42:37 -0500

    apex: Draft of project page

--- apex/app/projects/page.js
+++ apex/app/projects/page.js
@@ -1,30 +1,68 @@
-export default Projects;
+import Image from "next/image";
+
+import quantum from "@/assets/project_quantum.png";
+import oclrs from "@/assets/project_ocl-rs.png";
+import opensource from "@/assets/project_opensource.png";
+import Link from "next/link";
 
 export const metadata = {
   title: 'Projects',
   description: 'Wip',
 }
 
-function Projects() {
+
+export default function Projects() {
   return (
     <div className="px-4">
       <h1 className="text-2xl text-center font-bold py-8">Projects</h1>
 
       <div className="pb-4">
         <h2 className="text-xl">Featured</h2>
-        <p>* Quantum (wip)</p>
-        <p>* ocl-rs</p>
-        <p>* Open Source (wip)</p>
+        <BigBox name="Quantum" img={quantum} url="https://github.com/SkohTV/quantum" text="All-in-one Discord bot managing my discord and Youtube channel" />
+        <BigBox name="ocl-rs" img={oclrs} url="https://github.com/ISENLabs/ocl-rs" text="High-performance, fault-tolerant CDN for https://lazy.isenlabs.fr" />
+        <BigBox name="Open Source" img={opensource} url="https://github.com/SkohTV?tab=repositories&type=fork" text="Nixpkgs maintainer and active bpfilter contributor" />
       </div>
 
       <div className="pt-4">
         <h2 className="text-xl">Smaller</h2>
-        <p>* Dotfiles</p>
-        <p>* Remote Desktop</p>
-        <p>* skoh-dev</p>
-        <p>* skoh-tv</p>
+        <div className="flex flex-wrap">
+          <SmallBox name="Dotfiles" url="https://github.com/SkohTV/dotfiles" text="Personnal NixOS configuration files" />
+          <SmallBox name="Remote Desktop" url="https://github.com/SkohTV/remote-desktop/" text="Desktop-in-cloud setup, using AWS & other automations" />
+          <SmallBox name="skoh-dev" url="https://github.com/SkohTV/skoh-dev/" text="Personnal website & portfolio (this website)" />
+          <SmallBox name="skoh-tv" url="https://github.com/SkohTV/skoh-tv" text="Personnal linktree" />
+          <SmallBox name="excalidraw-exporter" url="https://github.com/SkohTV/excalidraw-exporter" text="Convert excalidraw files into svg via Github Actions" />
+          <SmallBox name="rbuild" url="https://github.com/SkohTV/rbuild" text="Work in progress" />
+        </div>
       </div>
 
     </div>
   );
+}
+
+
+
+function BigBox({ name, img, text, url }) {
+  return (
+    <Link href={url} className="flex bg-zinc-800/30 p-2 m-2 border border-transparent hover:border-zinc-700 duration-100 rounded-md w-150">
+      <div className="bg-zinc-800 p-1.5 rounded-md">
+        <Image alt={name} src={img} width={48} height={48}/>
+      </div>
+      <div className="flex flex-col px-4 justify-around py-1">
+        <div className="font-bold">{name}</div>
+        <div>{text}</div>
+      </div>
+    </Link>
+  )
+}
+
+
+function SmallBox({ name, text, url }) {
+  return (
+    <Link href={url} className="flex bg-zinc-800/30 p-2 m-2 border border-transparent hover:border-zinc-700 duration-100 rounded-md min-w-96 w-[32%]">
+      <div className="flex flex-col px-4 justify-around py-1">
+        <div className="font-bold">{name}</div>
+        <div>{text}</div>
+      </div>
+    </Link>
+  )
 }
